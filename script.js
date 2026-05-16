32b3942c0e595b5ef4c872257ac32d45ec86a8df
zuhamusallam <zuha_musallam@ahss.edu.jo>
2026-05-16 15:05:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function start(){
+    var x=document.getElementById("mytopnav");
+    if(x.className==="topnav"){
+        x.className+=" responsive";
+    }else{
+        x.className="topnav";
+    }
+}
